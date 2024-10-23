916f3d88abf2290a2ccc4d7309f63ff1c4e65473
Shivani Vijay <shivanivijayy@outlook.com>
2024-10-23 18:40:18 -0400

    Update server.js

--- Assignments/Assignment-03/server.js
+++ Assignments/Assignment-03/server.js
@@ -82,7 +82,7 @@ const express = require("express");
 const projectData = require("./modules/projects");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const HTTP_PORT = process.env.PORT || 3000; 
 
 app.use(express.static('public')); 
 projectData
