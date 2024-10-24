d8c92b937ec926b61d3b97a9fde1100f3d8341ea
Shivani Vijay <shivanivijayy@outlook.com>
2024-10-23 20:51:57 -0400

    Update server.js

--- Assignments/Assignment-03/server.js
+++ Assignments/Assignment-03/server.js
@@ -1,91 +1,22 @@
 /********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Shivani Vijay Student ID: 148493232 Date: 05/10/2024
+*  Name: Shivani Vijay Student ID: 148493232 Date: 23/10/2024
 *
 ********************************************************************************/
-/*const expr
-// imports express to handle server
-const express = require("express");
-const projectData = require("./modules/projects"); // import project data
-
-const app = express(); // initializes an express app
-const HTTP_PORT = process.env.PORT || 3000; 
-// http port sets port no if not default port is 3000
-
-
-// Initializes project data before starting the server
-// once initialization is successful, the server starts listening on specified port
-projectData
-  .initialize()
-  .then(() => {
-    app.listen(HTTP_PORT, () => {
-      console.log(`Server is listening on port ${HTTP_PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Unable to start the server:", err);
-  });
-
-// GET "/" get requests data from server
-// this route simply sends a message with name & id when home page is acccesed
-app.get("/", (req, res) => {
-  res.send("Assignment 2: Shivani Vijay - 148493232");
-});
-
-// GET "/solutions/projects"
-// returns all available projects in JSON format
-app.get("/solutions/projects", (req, res) => {
-  projectData
-    .getAllProjects() // calls this function if success it responds with list of porjects
-    .then((projects) => {
-      res.json(projects);
-    })
-    .catch((err) => { // if error then it sends a code of 500
-      res.status(500).send(err);
-    });
-});
-
-// GET "/solutions/projects/id-demo"
-// fetches & returns project with ID 9
-app.get("/solutions/projects/id-demo", (req, res) => {
-  projectData
-    .getProjectById(9) // calls this and returns project in json format
-    .then((project) => {
-      res.json(project);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-// GET "/solutions/projects/sector-demo"
-// returns all projects related to agriculture sector
-app.get("/solutions/projects/sector-demo", (req, res) => {
-  projectData
-    .getProjectsBySector("agriculture") // calls this and prints in json
-    .then((projects) => {
-      res.json(projects);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-*/
-
 
 const express = require("express");
 const projectData = require("./modules/projects");
 
 const app = express();
-
 const HTTP_PORT = process.env.PORT || 3000;
-app.use(express.static('public')); // Make public folder static
+const port = process.env.PORT || 3000;
+app.use(express.static('public'));
 
 projectData
   .initialize()
@@ -100,12 +31,12 @@ projectData
 
 // GET "/"
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/home.html"); // Serve home.html
+  res.sendFile(__dirname + "/views/home.html"); // Serves the home.html
 });
 
 // GET "/about"
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html"); // Serve about.html
+  res.sendFile(__dirname + "/views/about.html"); // Serve the about.html
 });
 
 // GET "/solutions/projects"
@@ -144,6 +75,5 @@ app.get("/solutions/projects/:id", (req, res) => {
 
 // Custom 404 error page
 app.use((req, res) => {
-  res.status(404).sendFile(__dirname + "/views/404.html"); // Serve 404.html
+  res.status(404).sendFile(__dirname + "/views/404.html"); // Serves 404.html
 });
-
